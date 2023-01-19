96c385324415f76a53ff20e6366b5cafbdb1eb25
junxtar <aa1870088@naver.com>
2023-01-19 20:35:32 +0900

    feat: signview

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -62,24 +62,28 @@
             <v-col cols="12" md="6">
               <div style="padding: 30px 0"></div>
               <h3 class="text-center">Welcome to Nodrain.</h3>
-              <img
-                class="center"
-                src="../assets/nobrainlogo.png"
-                alt="NobrainLogo"
-                style="
-                  height: 200px;
-                  width: 200px;
-                  object-fit: cover;
-                  margin-left: 29%;
-                "
-              />
+              <div class="text-center">
+                <img
+                  class="center"
+                  src="../assets/nobrainlogo.png"
+                  alt="NobrainLogo"
+                  style="width: 200px"
+                />
+              </div>
+
               <div style="text-align: center">
                 <v-card-text class="white--text">
                   <h3 class="text-center">Don't Have an Account Yet?</h3>
                   <h6 class="text-center">Sign up here</h6>
                 </v-card-text>
                 <div class="text-center">
-                  <v-btn absolute="true" tile outlined dark to="/signup"
+                  <v-btn
+                    absolute="true"
+                    tile
+                    outlined
+                    dark
+                    to="/signup"
+                    color="#BBDEFB"
                     >Sing up
                   </v-btn>
                 </div>
