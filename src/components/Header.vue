d5449e6274eacf9a8e29130471ab4700d99de43b
yuntaeLee <leeyt1201@gmail.com>
2023-01-29 20:21:15 +0900

    feat: change search bar design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,13 +3,12 @@
       app
       flat
       height="90"
+      id="app-bar"
   >
-    <v-container class="py-0 fill-height">
-      <v-avatar
-          class="mr-12"
-          color="grey darken-1"
-          size="50"
-      ></v-avatar>
+    <v-container class="py-2 fill-height">
+<!--      <v-avatar color="info">-->
+      <v-icon icon="mdi-account-circle" size="60" id="user-img" color="black"></v-icon>
+<!--      </v-avatar>-->
 
       <v-btn
           v-for="link in links"
@@ -21,15 +20,24 @@
 
       <v-spacer></v-spacer>
 
-      <v-responsive max-width="260">
-        <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-        ></v-text-field>
-      </v-responsive>
+      <v-card
+          class="mx-auto"
+          color="#dedada"
+          min-width="600"
+      >
+        <v-card-text>
+          <v-text-field
+              :loading="loading"
+              density="compact"
+              variant="solo"
+              label="Search bookmarks"
+              append-inner-icon="mdi-magnify"
+              single-line
+              hide-details
+              @click:append-inner="onClick"
+          ></v-text-field>
+        </v-card-text>
+      </v-card>
     </v-container>
   </v-app-bar>
 </template>
@@ -44,10 +52,31 @@
         'Profile',
         'Updates',
       ],
-    })
+
+      loaded: false,
+      loading: false,
+    }),
+
+    methods: {
+      onClick() {
+        this.loading = true
+
+        setTimeout(() => {
+          this.loading = false;
+          this.loaded = true;
+        }, 2000)
+      }
+    }
   }
 </script>
 
 <style scoped>
+#app-bar {
+  background: #dedada;
+}
 
+#user-img {
+  margin-left: 10px;
+  margin-right: 50px;
+}
 </style>
