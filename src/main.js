5e52edcd0aa84b1f4abc46a380db136a131ec6f3
yuntaeLee <leeyt1201@gmail.com>
2023-02-01 17:21:44 +0900

    feat: Add vue3-tags-input

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import Vue3TagsInput from 'vue3-tags-input'
 
 loadFonts();
 
-const app = createApp(App).use(router).use(vuetify).mount("#app");
+const app = createApp(App).use(router).use(vuetify).component('vue3-tags-input', Vue3TagsInput).mount("#app");
