c5e5ec4870f082624677a0955dc5713223d6d6dd
yuntaeLee <leeyt1201@gmail.com>
2023-02-03 22:48:27 +0900

    [#16] refactor: SignUpDialog
    close #16

--- src/components/dialog/SignUpDialog.vue
+++ src/components/dialog/SignUpDialog.vue
@@ -1,39 +1,35 @@
 <template>
-  <!-- v-model="dialogid" -->
-  <v-dialog activator="parent" transition="dialog-top-transition">
-    <!-- v-if="isExistsId === true" -->
-    <v-card class="mx-auto" max-width="500" style="width: 500px; height: 200px">
+  <v-dialog v-model="dialog.isShow" activator="parent" transition="dialog-top-transition">
+    <v-card v-if="dialog.isExist" class="mx-auto" max-width="500" style="width: 500px; height: 200px">
       <v-card-title class="text-center" style="margin-top: 10%; color: red">
-        {{ text }}
-        <!-- {{ test }} -->
+        {{ dialog.title }}
       </v-card-title>
       <v-card-actions style="margin-top: 10%">
-        <v-btn color="black" block>Close </v-btn>
-        <!-- @click="dialogid = false" -->
+        <v-btn color="black" block @click="dialog.isShow = false">Close </v-btn>
       </v-card-actions>
     </v-card>
 
-    <!-- v-else -->
-    <v-card class="mx-auto" max-width="500" style="width: 500px; height: 200px">
+    <v-card v-else class="mx-auto" max-width="500" style="width: 500px; height: 200px">
       <v-card-title class="text-center" style="margin-top: 10%; color: #3333ff">
-        사용가능한 아이디 입니다.
+        {{ dialog.title }}
       </v-card-title>
       <v-card-actions style="margin-top: 10%">
-        <v-btn color="black" block>Close</v-btn>
-        <!-- @click="dialogid = false" -->
+        <v-btn color="black" block @click="dialog.isShow = false">Close</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 <script>
-import { ref } from "vue";
 export default {
-  props: ["text"],
+  name: "SignDialog",
+  props: {
+    title: "",
+    dialog: {
+      title: "",
+      isShow: false,
+      isExist: false,
+    }
+  }
 };
-// const props = defineProps({
-//   text: String,
-// });
-
-const test = ref("안녕!!");
 </script>
 <style></style>
