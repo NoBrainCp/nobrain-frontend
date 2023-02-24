81a9459205adf57294c29ed8753b441cc5f58327
yuntaeLee <leeyt1201@gmail.com>
2023-02-24 19:57:10 +0900

    feat: Add vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import { createApp } from "vue";
+import { createStore } from "vuex";
 import App from "./App.vue";
 import router from "./router";
+import vueCookies from "vue-cookies"
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import Vue3TagsInput from 'vue3-tags-input'
 
 loadFonts();
 
-const app = createApp(App).use(router).use(vuetify).component('vue3-tags-input', Vue3TagsInput).mount("#app");
+const store = createStore({
+    state() {
+        return {
+            count: 0
+        }
+    },
+    mutations: {
+        increment (state) {
+            state.count++
+        }
+    }
+})
+
+const app = createApp(App).use(router)
+    .use(vueCookies)
+    .use(store)
+    .use(vuetify)
+    .component('vue3-tags-input', Vue3TagsInput)
+    .mount("#app");
