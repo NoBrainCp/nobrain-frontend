873c31328b6bc3a01ef218fe1d3cec78fc3412ba
junxtar <aa1870088@naver.com>
2023-03-07 23:21:38 +0900

    [#49] feat:Implemented user information page

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app id="inspire">
     <Headed/>
-
     <Side/>
-
+    <v-main class="main">
+      <Profile/>
+    </v-main>
     <TagBar class="tag-bar"/>
 
   </v-app>
@@ -13,9 +14,10 @@
 import Side from "../components/Side.vue";
 import Headed from "../components/Head.vue";
 import TagBar from "../components/TagBar.vue";
+import Profile from "../components/Profile.vue";
 
 export default {
-  components: {TagBar, Headed, Side},
+  components: {Profile, TagBar, Headed, Side},
   name: 'userprofile',
 
   data: () => ({
@@ -26,5 +28,11 @@ export default {
 </script>
 
 <style scoped>
+
+.main {
+  margin-top: 35px;
+  margin-bottom: 35px;
+  margin-left: 35px;
+}
 
 </style>
