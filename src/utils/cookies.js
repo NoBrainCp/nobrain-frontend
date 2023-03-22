adb4d00f17c1997d9b868a8ee79745d876d6dc4d
junxtar <aa1870088@naver.com>
2023-03-22 13:32:00 +0900

    [#81] fix: 회원 탈퇴시 accessToken 미삭제 버그 수정

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -76,6 +76,10 @@ function deleteUsernameFromCookie() {
     document.cookie = `username=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 }
 
+function deleteAccessTokenFromCookie() {
+    document.cookie = `accessToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+}
+
 function deleteCookie(value) {
     document.cookie = `${value}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 }
@@ -98,5 +102,6 @@ export {
     getRefreshTokenFromCookie,
 
     deleteUsernameFromCookie,
+    deleteAccessTokenFromCookie,
     deleteCookie,
 };
