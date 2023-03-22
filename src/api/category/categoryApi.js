495207838e06a0be242cc52035bfafc4bbe37125
junxtar <aa1870088@naver.com>
2023-03-22 20:36:09 +0900

    [#68] feat: 카테고리 단일 건 조회 API 추가

--- src/api/category/categoryApi.js
+++ src/api/category/categoryApi.js
@@ -1,21 +1,25 @@
 import {category} from "../index";
 
+export function getCategory(categoryId) {
+    return category.get(`/category/${categoryId}`);
+}
+
 export function getCategories(username) {
-    return category.get(`user/${username}/categories`);
+    return category.get(`/user/${username}/categories`);
 }
 
 export function getCategoryByBookmarkId(bookmarkId) {
-    return category.get(`bookmark/${bookmarkId}/categoryName`);
+    return category.get(`/bookmark/${bookmarkId}/categoryName`);
 }
 
 export function addCategory(username, categoryData) {
-    return category.post(`${username}/category`, categoryData);
+    return category.post(`/${username}/category`, categoryData);
 }
 
 export function updateCategory(username, categoryName, categoryData) {
-    return category.put(`${username}/category/${categoryName}`, categoryData);
+    return category.put(`/${username}/category/${categoryName}`, categoryData);
 }
 
 export function deleteCategory(username, categoryName) {
-    return category.delete(`${username}/category/${categoryName}`);
+    return category.delete(`/${username}/category/${categoryName}`);
 }
