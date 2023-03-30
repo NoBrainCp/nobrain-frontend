e3872c32cdcaa606bd189bee083f858997e716e9
junxtar <aa1870088@naver.com>
2023-03-30 14:57:42 +0900

    [#105] refactor: [CategoryBar.vue] 기존: optionsAPI + compositionAPI 변경: compositionAPI

--- src/components/main/CategoryBar.vue
+++ src/components/main/CategoryBar.vue
@@ -2,16 +2,16 @@
   <v-app-bar class="app-bar" style="position: fixed" flat border>
     <div class="category-title">
       <v-icon class="mr-2">mdi mdi-bookshelf</v-icon>
-      {{ data.category.name }}
+      {{ category.name }}
     </div>
 
     <div class="category-description">
-      {{ data.category.description }}
+      {{ category.description }}
     </div>
 
     <v-spacer/>
     <v-btn
-        v-if="data.isMe && !data.isAll"
+        v-if="isMe && !isAll"
         class="btn"
         color="blue"
         elevation="3"
@@ -22,24 +22,31 @@
     </v-btn>
     <CategoryDialog
         :categoryDialog="categoryDialog"
-        @submit="updateCategory"/>
+        @submit="updateCategoryData"/>
     <v-btn
-        v-if="data.isMe && !data.isAll"
+        v-if="isMe && !isAll"
         class="btn"
         color="red-accent-4"
         elevation="3"
         prepend-icon="mdi mdi-delete"
         border
-        @click="this.confirmObj.dialog=true">
+        @click="confirmObj.dialog=true">
       삭제
     </v-btn>
     <ConfirmDialog
-        v-bind:confirmObj="confirmObj"
-        @delete="deleteCategory"/>
+        :confirmObj="confirmObj"
+        @delete="deleteCategoryData"/>
   </v-app-bar>
 </template>
 
-<script>
+<script setup>
+import router from "../../router";
+import {categoryStore} from "../../store/category/category";
+import {favoritesStore} from "../../store/favorites/favorites";
+import {bookmarkStore} from "../../store/bookmark/bookmark";
+import {privatesStore} from "../../store/privates/privates";
+import ConfirmDialog from "../dialog/ConfirmDialog.vue";
+import CategoryDialog from "../dialog/CategoryDialog.vue";
 import {onMounted, ref, watch} from "vue";
 import {useRoute} from "vue-router";
 import {
@@ -47,133 +54,103 @@ import {
   getUsernameFromCookie
 } from "../../utils/cookies";
 import {deleteCategory, getCategory, updateCategory} from "../../api/category/categoryApi";
-import {categoryStore} from "../../store/category/category";
-import router from "../../router";
-import ConfirmDialog from "../dialog/ConfirmDialog.vue";
-import CategoryDialog from "../dialog/CategoryDialog.vue";
-import {favoritesStore} from "../../store/favorites/favorites";
 import {updateAllBookmarksToPrivate} from "../../api/bookmark/bookmarkApi";
-import {bookmarkStore} from "../../store/bookmark/bookmark";
-import {privatesStore} from "../../store/privates/privates";
 
-export default {
-  name: 'CategoryBar',
-  components: {ConfirmDialog, CategoryDialog},
+const route = useRoute();
 
-  data: () => ({
-    route: useRoute(),
+const confirmObj = ref({
+  dialog: false,
+  title: "카테고리 삭제",
+  text: "정말 카테고리를 삭제하시겠습니까?<br/>삭제하시면 관련된 북마크들까지 모두 삭제됩니다.",
+  buttonText: "삭제"
+});
 
-    confirmObj: {
-      title: "카테고리 삭제",
-      text: "정말 카테고리를 삭제하시겠습니까?<br/>삭제하시면 관련된 북마크들까지 모두 삭제됩니다.",
-      dialog: false,
-      buttonText: "삭제"
-    },
-  }),
+const categoryDialog = ref({
+  dialog: false,
+  title: "카테고리 수정",
+  btnName: "수정",
+  name: "",
+  description: "",
+  isPublic: "",
+});
 
-  setup() {
-    const route = useRoute();
-    const data = ref({
-      category: {
-        name: "",
-        description: "",
-        public: "",
-      },
-      isMe: route.params.username === getUsernameFromCookie(),
-      isAll: false,
-    });
+const category = ref({
+  name: "",
+  description: "",
+  public: "",
+});
 
-    const categoryDialog = ref({
-      dialog: false,
-      title: "카테고리 수정",
-      btnName: "수정",
-      name: String,
-      description: String,
-      isPublic: Boolean,
-    });
+const isMe = ref(route.params.username === getUsernameFromCookie());
+const isAll = ref(false);
 
-    async function getCategoryList() {
-      try {
-        const username = route.params.username;
-        const categoryName = route.params.category;
-
-        if (!categoryName || categoryName === "starred" || categoryName === 'private') {
-          if (categoryName === 'starred') {
-            data.value.category.name = "즐겨찾기";
-          } else if(categoryName === 'private') {
-            data.value.category.name = '비공개';
-          } else {
-            data.value.category.name = '전체 북마크';
-          }
-          data.value.category.description = "";
-          data.value.isAll = true;
-          return;
-        }
-        const response = await getCategory(username, categoryName);
-        data.value.category = response.data.data;
-        data.value.isAll = false;
-
-      } catch (error) {
-        console.log(error);
-      }
+const userId = ref(getUserIdFromCookie());
+const getCategoryList = async() => {
+  const username = route.params.username;
+  const categoryName = route.params.category;
+  if (!categoryName || categoryName === "starred" || categoryName === 'private') {
+    if (categoryName === 'starred') {
+      category.value.name = "즐겨찾기";
+    } else if (categoryName === 'private') {
+      category.value.name = "비공개";
+    } else {
+      category.value.name = "전체 북마크";
     }
-
-    watch(() => route.params.category, () => {
-      getCategoryList();
-    });
-
-    onMounted(() => {
-      getCategoryList();
-    });
-
-    return {
-      data,
-      categoryDialog
-    };
-  },
-
-  methods: {
-    updateClick() {
-      this.categoryDialog.dialog = true
-      this.categoryDialog.name = this.data.category.name;
-      this.categoryDialog.description = this.data.category.description;
-      this.categoryDialog.isPublic = this.data.category.public;
-    },
-
-    async updateCategory(category) {
-      try {
-        const categoryName = category.name;
-        const categoryOriginName = this.data.category.name;
-        const userId = getUserIdFromCookie();
-        this.data.category.name = category.name;
-        this.data.category.description = category.description;
-        this.data.category.public = category.isPublic;
-        await updateCategory(getUsernameFromCookie(), categoryOriginName, category);
-        await updateAllBookmarksToPrivate(userId, this.data.category.name);
-        await router.push(`/${getUsernameFromCookie()}/${categoryName}`);
-        categoryStore.state.status = !categoryStore.state.status;
-        bookmarkStore.state.status = !bookmarkStore.state.status;
-        privatesStore.state.status = !privatesStore.state.status;
-      } catch (error) {
-        alert(error.response);
-      }
-    },
-
-    async deleteCategory() {
-      try {
-        await deleteCategory(getUsernameFromCookie(), this.data.category.name);
-        await router.push(`/${getUsernameFromCookie()}`);
-        categoryStore.state.status = !categoryStore.state.status;
-        favoritesStore.state.status = !favoritesStore.state.status;
-        privatesStore.state.status = !privatesStore.state.status;
-        bookmarkStore.state.status = !bookmarkStore.state.status;
-      } catch (error) {
-        alert(error.response.data.message);
-      }
-    },
+    category.value.description = "";
+    isAll.value = true;
+    return;
   }
-}
+  await getCategory(username, categoryName).then((response) => {
+    category.value = response.data.data;
+    isAll.value = false;
+  }).catch((error) => {
+    console.log(error);
+  })
+};
 
+watch(() => route.params.category, () => {
+  getCategoryList();
+});
+
+onMounted(() => {
+  getCategoryList();
+});
+
+
+const updateClick = () => {
+  categoryDialog.value.dialog = true;
+  categoryDialog.value.name = category.value.name;
+  categoryDialog.value.description = category.value.description;
+  categoryDialog.value.isPublic = category.value.public;
+};
+
+const updateCategoryData = async(categoryObj) => {
+  const username = getUsernameFromCookie();
+  const categoryOriginName = category.value.name;
+  category.value.name = categoryObj.name;
+  category.value.description = categoryObj.description;
+  category.value.public = categoryObj.isPublic;
+  await updateCategory(username, categoryOriginName, categoryObj).then(() => {
+    updateAllBookmarksToPrivate(userId.value, categoryOriginName);
+    router.push(`/${username}/${category.value.name}`);
+    categoryStore.state.status = !categoryStore.state.status;
+    bookmarkStore.state.status = !bookmarkStore.state.status;
+    privatesStore.state.status = !privatesStore.state.status;
+  }).catch((error) => {
+    alert(error.response);
+  })
+};
+
+const deleteCategoryData = async() => {
+  await deleteCategory(getUsernameFromCookie(), category.value.name).then(() => {
+    router.push(`/${getUsernameFromCookie()}`);
+    categoryStore.state.status = !categoryStore.state.status;
+    favoritesStore.state.status = !favoritesStore.state.status;
+    privatesStore.state.status = !privatesStore.state.status;
+    bookmarkStore.state.status = !bookmarkStore.state.status;
+  }).catch((error) => {
+    alert(error.response.data.message);
+  })
+};
 </script>
 
 <style scoped>
