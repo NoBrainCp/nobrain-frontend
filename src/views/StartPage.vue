ef4987288f73a41367a89ec94ce9fc2546ebcfdd
junxtar <aa1870088@naver.com>
2023-04-05 17:52:40 +0900

    [#113] feat: startPage 컴포넌트 생성

---
+++ src/views/StartPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header/>
+    <Body/>
+    <Function/>
+  </div>
+
+</template>
+<script setup>
+import Header from "../components/start/Header.vue";
+import Body from "../components/start/Body.vue";
+import Function from "../components/start/Function.vue";
+
+</script>
+
+<style scoped>
+
+
+</style>
