d5c63c7fd22e94dbf1cc2b2471a08078a440d48d
junxtar <aa1870088@naver.com>
2023-04-07 00:58:08 +0900

    [#113] feat: 페이지 상단 이동 버튼 구현

--- src/components/start/Footer.vue
+++ src/components/start/Footer.vue
@@ -1,7 +1,31 @@
 <template>
+  <div class="btn">
+    <v-icon
+        size="50"
+        @click="goTop"
+    >mdi-arrow-up-circle-outline
+    </v-icon>
+  </div>
   <v-footer class="bg-blue-lighten-4">
     <v-col class="text-center mt-4" cols="12">
       {{ new Date().getFullYear() }} — <strong>Nobrain</strong>
     </v-col>
   </v-footer>
 </template>
+
+<script setup>
+
+const goTop = () => {
+  window.scrollTo({top:0, behavior: 'smooth'});
+};
+
+</script>
+<style scoped>
+.btn {
+  cursor: pointer;
+  position: fixed;
+  bottom: 100px;
+  right: 100px;
+  z-index: 12;
+}
+</style>
