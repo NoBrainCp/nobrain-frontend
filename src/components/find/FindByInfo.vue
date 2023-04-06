684a3ce5787d724dc2ab6ecbc2725d1a862f8873
junxtar <aa1870088@naver.com>
2023-04-06 20:44:30 +0900

    [#113] refactor: 아이디 찾기 및 비밀번호 찾기 컴포넌트 재구성

---
+++ src/components/find/FindByInfo.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card-text class="find-by-info">
+    <h2 class="body-card-title text-h3 font-weight-bold text-center mt-3 mb-8">
+      Nobrain
+    </h2>
+    <h1 class="body-card-sub-title font-weight-regular ml-6 mb-n2">
+      {{infoText}}
+    </h1>
+
+    <FindByCertifiedCard/>
+  </v-card-text>
+</template>
+
+<script setup>
+import FindByCertifiedCard from "../certifiedcard/FindByCertifiedCard.vue";
+import {onMounted, ref} from "vue";
+import {store} from "../../store";
+const infoText = ref("");
+
+onMounted(() => {
+  if (!store.state.loginId) {
+    infoText.value = "아이디 찾기";
+  } else {
+    infoText.value = "비밀번호 찾기";
+  }
+});
+
+</script>
+
+<style scoped>
+
+.find-by-info {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
+
+.body-card-title {
+  color: #0363f4;
+  top: 2%;
+  position: relative;
+}
+
+.body-card-sub-title {
+  left: 2%;
+  position: relative;
+}
+
+
+hr {
+  background-color: rgba(56, 51, 51, 0.257);
+  height: 2pt;
+  margin-bottom: 0.5%;
+}
+
+</style>
---
+++ src/components/find/ForgetPassword.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-card-text class="justify-center align-center">
+    <h2 class="body-card-title text-h3 font-weight-bold text-center">
+      Nobrain
+    </h2>
+      <h2 class="body-card-sub-title font-weight-regular text-center">
+        비밀번호를 찾고자 하는 아이디를 입력해주세요.
+      </h2>
+      <v-text-field
+          class="card-text-id"
+          variant="outlined"
+          prepend-inner-icon="mdi-account"
+          v-model="userId"
+          label="Id"
+          color="blue"
+          @keydown.enter="checkDuplicationId(userId)"
+      />
+      <v-btn
+          class="card-button text-h6"
+          color="blue"
+          variant="outlined"
+          @click="checkDuplicationId(userId)"
+      >
+        다음
+      </v-btn>
+
+      <span class="link-span font-weight-thin">
+        아이디가 기억나지 않는다면?
+        <h5 class="find-by-id text-blue-accent-3 font-weight-regular" @click="setWindow">
+         아이디 찾기
+      </h5>
+      </span>
+
+
+  </v-card-text>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+import {existsLoginId} from "../../api/user/userApi";
+import {store} from "../../store";
+
+const userId = ref("");
+
+const checkDuplicationId = async (userId) => {
+  await existsLoginId(userId).then((response) => {
+    if (response.data.data) {
+      store.commit('setLoginId', userId);
+      store.commit('setWindow', "findByInfo");
+    } else {
+      alert("아이디를 확인해주세요.");
+    }
+  }).catch((error) => {
+    console.log(error);
+  })
+};
+
+const setWindow = () => {
+  store.commit('setWindow', "findByInfo");
+};
+
+</script>
+
+<style scoped>
+.body-card-title {
+  color: #0363f4;
+  top: 10%;
+  width: 70%;
+  left: 15%;
+  position: absolute;
+}
+
+.body-card-sub-title {
+  top: 20%;
+  width: 70%;
+  left: 15%;
+  position: absolute;
+}
+
+.card-text-id {
+  top: 40%;
+  left: 15%;
+  width: 70%;
+  position: absolute;
+}
+
+.card-button {
+  left: 15%;
+  width: 70%;
+  top: 55%;
+  height: 8%;
+  position: absolute;
+}
+
+.link-span {
+  left: 15%;
+  top: 75%;
+  position: absolute;
+}
+
+span {
+  font-size: 13pt;
+}
+
+.find-by-id {
+  cursor: pointer;
+}
+
+</style>
