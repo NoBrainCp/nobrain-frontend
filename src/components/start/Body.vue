9e178cdedb1b7cdfe36dfaf3c5576a09e01bea60
yoon <leeyt1201@gmail.com>
2023-04-06 19:10:46 +0900

    [#116] style: body component UI 개선

--- src/components/start/Body.vue
+++ src/components/start/Body.vue
@@ -1,33 +1,34 @@
 <template>
   <v-parallax
-      class="parallax"
-      src="/../src/assets/images/start-page-header.jpg"
+    class="parallax"
+    src="/../src/assets/images/start-page-header.jpg"
   >
-  <v-row class="d-flex">
-    <v-col cols="12" sm="7">
-      <div class="body-text ml-8">
-        <h3 class="text-h3 font-weight-thin mb-5">
-          자신의 북마크를 기록하고 공유하세요!
-        </h3>
-        <h4>
-          Build your own bookmark
-        </h4>
-      </div>
-    </v-col>
-    <v-col cols="12" sm="5">
-
-      <v-card class="body-card d-flex">
-        <v-card-text>
-          <h2 class="body-card-title text-h3 font-weight-bold text-center mt-10">Nobrain</h2>
-          <div class="body-card-text">
-            <v-text-field
+    <v-row class="d-flex">
+      <v-col cols="12" sm="7">
+        <div class="body-text ml-8">
+          <h3 class="text-h3 font-weight-thin mb-5">
+            자신의 북마크를 기록하고 공유하세요!
+          </h3>
+          <h4>Build your own bookmark</h4>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="5">
+        <v-card class="body-card d-flex">
+          <v-card-text>
+            <h2
+              class="body-card-title text-h3 font-weight-bold text-center mt-10"
+            >
+              Nobrain
+            </h2>
+            <div class="body-card-text">
+              <v-text-field
                 label="Id"
                 bg-color="white"
                 color="blue"
                 variant="outlined"
                 v-model="userData.loginId"
-            />
-            <v-text-field
+              />
+              <v-text-field
                 class="mt-1"
                 label="Password"
                 bg-color="white"
@@ -36,69 +37,67 @@
                 variant="outlined"
                 @keydown.enter="signIn"
                 v-model="userData.password"
-            />
-            <v-row class="mt-2 mb-5">
-              <v-checkbox
+              />
+              <v-row class="mt-2 mb-5">
+                <v-checkbox
                   label="로그인 상태 유지"
                   class="mt-n5"
                   color="blue"
                   v-model="isRememberId"
-              />
-            </v-row>
-            <v-btn
+                />
+              </v-row>
+              <v-btn
                 color="blue"
                 class="mt-2 mb-3 login-button"
                 variant="outlined"
                 block
                 @click="signIn"
-            >
-              로그인
-            </v-btn>
-<!--            :href="googleSignUrl"-->
+              >
+                로그인
+              </v-btn>
+              <!--            :href="googleSignUrl"-->
 
-            <a
+              <a
                 class="caption text"
                 id="forgetPassword"
                 href="/forget-password"
-            >
-              아이디 찾기 / 비밀번호 찾기
-            </a>
-            <div class="body-card-oauth-button mt-8">
-              <img
+              >
+                아이디 찾기 / 비밀번호 찾기
+              </a>
+              <div class="body-card-oauth-button mt-8">
+                <img
                   src="src/assets/images/google-logo.png"
                   class="google-logo"
                   alt="google"
                   @click="googleLogin"
-              />
-              <img
+                />
+                <img
                   src="src/assets/images/naver-logo.png"
                   class="naver-logo"
                   alt="naver"
                   @click="naverLogin"
-              />
-              <img
+                />
+                <img
                   src="src/assets/images/kakao-logo.png"
                   class="kakao-logo"
                   alt="kakao"
                   @click="kakaoLogin"
-              />
+                />
+              </div>
             </div>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-parallax>
-  <div class="space"/>
-
+  <div class="space" />
 </template>
 
 <script setup>
-import {ref} from "vue";
-import {useRoute} from "vue-router";
-import {store} from "../../store";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { store } from "../../store";
 import router from "../../router";
-
 
 const userData = ref({
   loginId: "",
@@ -111,29 +110,37 @@ const errorObj = ref({
   text: "",
 });
 
-const signIn = async() => {
-  await store.dispatch('signIn', userData.value).then((response) => {
-    router.replace(`/${response.username}`);
-  }).catch((error) => {
-    console.log(error);
-    errorObj.value.title = "로그인 오류";
-    errorObj.value.text = error.response.data.message;
-    isError.value = true;
-  })
+const signIn = async () => {
+  await store
+    .dispatch("signIn", userData.value)
+    .then((response) => {
+      router.replace(`/${response.username}`);
+    })
+    .catch((error) => {
+      console.log(error);
+      errorObj.value.title = "로그인 오류";
+      errorObj.value.text = error.response.data.message;
+      isError.value = true;
+    });
 };
 
-const googleSignUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=' +
-    `${import.meta.env.VITE_APP_GOOGLE_CLIENT_ID}` +
-    '&redirect_uri=' +
-    `${import.meta.env.VITE_APP_GOOGLE_REDIRECT_URL}` +
-    '&response_type=code' +
-    '&scope=email profile';
+const googleSignUrl =
+  "https://accounts.google.com/o/oauth2/v2/auth?client_id=" +
+  `${import.meta.env.VITE_APP_GOOGLE_CLIENT_ID}` +
+  "&redirect_uri=" +
+  `${import.meta.env.VITE_APP_GOOGLE_REDIRECT_URL}` +
+  "&response_type=code" +
+  "&scope=email profile";
 
 const route = useRoute();
 const isRememberId = ref(true);
 const isError = ref(false);
 const oauthUserInfo = ref({});
 
+onMounted(() => {
+  let parallax = document.querySelector(".parallax");
+  console.log(parallax);
+});
 </script>
 
 <style scoped>
@@ -141,6 +148,7 @@ const oauthUserInfo = ref({});
   weight: 1800px;
   height: 800px;
   position: relative;
+  transition: all 3s;
 }
 
 .body-text {
@@ -171,7 +179,7 @@ const oauthUserInfo = ref({});
 }
 
 .space {
-  height: 100px
+  height: 100px;
 }
 
 .login-button {
@@ -179,9 +187,7 @@ const oauthUserInfo = ref({});
 }
 
 .body-card-oauth-button {
-
   left: 20%;
-
 }
 
 .google-logo {
@@ -204,5 +210,4 @@ const oauthUserInfo = ref({});
   cursor: pointer;
   left: 65%;
 }
-
 </style>
