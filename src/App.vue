42f8af9e3d1e456f75af07e56af2442b6c6e3e08
junxtar <aa1870088@naver.com>
2023-04-08 15:01:10 +0900

    [#113] feat: 폰트 스타일 추가 및 적용

--- src/App.vue
+++ src/App.vue
@@ -6,50 +6,23 @@
   </v-app>
 </template>
 
-<!-- <script>
-export default {
-  name: "App",
-
-  data: () => ({
-    //
-  }),
-};
-</script> -->
-
 <script>
-export default {
-  data: (vm) => ({
-    date: new Date().toISOString().substr(0, 10),
-    dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
-    menu1: false,
-    menu2: false,
-  }),
 
-  computed: {
-    computedDateFormatted() {
-      return this.formatDate(this.date);
-    },
-  },
-
-  watch: {
-    date(val) {
-      this.dateFormatted = this.formatDate(this.date);
-    },
-  },
-
-  methods: {
-    formatDate(date) {
-      if (!date) return null;
-
-      const [year, month, day] = date.split("-");
-      return `${month}/${day}/${year}`;
-    },
-    parseDate(date) {
-      if (!date) return null;
-
-      const [month, day, year] = date.split("/");
-      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
-    },
-  },
-};
 </script>
+
+<style>
+@font-face {
+  font-family: 'neoFont';
+  src: url('src/assets/fonts/neoFont.ttf') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'doveFont';
+  src: url('src/assets/fonts/doveMayo.ttf') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+
+</style>
