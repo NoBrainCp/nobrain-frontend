99f52c92ac49c8497a3459334de6f204c9f78d89
junxtar <aa1870088@naver.com>
2023-04-11 15:01:09 +0900

    [#125] refactor: authApi code refactor

--- src/api/auth/authApi.js
+++ src/api/auth/authApi.js
@@ -1,33 +1,44 @@
-import {instance} from "../index";
+import {auth, noAuthInstance} from "../index";
+
+export function login(loginData) {
+    return noAuthInstance.post(`/auth/login`, loginData);
+}
+
+export function logout(refreshToken) {
+    return auth.post(`/logout`, refreshToken);
+}
+export function generateAccessToken(refreshToken) {
+    return noAuthInstance.post(`/auth/access-token`, refreshToken);
+}
 
 export function sendAuthenticationPhoneNumber(phoneNumber) {
-    return instance.get(`/phone/${phoneNumber}/auth-code`);
+    return noAuthInstance.get(`/phone/${phoneNumber}/auth-code`);
 }
 
 export function sendAuthenticationEmail(email) {
-    return instance.get(`/mail/${email}/auth-code`);
+    return noAuthInstance.get(`/mail/${email}/auth-code`);
 }
 
 export function sendEmailAndLoginId(email, authCode) {
-    return instance.post(`/mail/${email}/auth-code/login-id`, {
+    return noAuthInstance.post(`/mail/${email}/auth-code/login-id`, {
         code: authCode
     });
 }
 
 export function sendMessageAndLoginId(phoneNumber, authCode) {
-    return instance.post(`phone/${phoneNumber}/auth-code/login-id`, {
+    return noAuthInstance.post(`phone/${phoneNumber}/auth-code/login-id`, {
         code: authCode
     });
 }
 
 export function sendMailAndPassword(email, authCode) {
-    return instance.post(`mail/${email}/auth-code/password`, {
+    return noAuthInstance.post(`mail/${email}/auth-code/password`, {
         code: authCode
     });
 }
 
 export function sendMessageAndPassword(phoneNumber, authCode) {
-    return instance.post(`phone/${phoneNumber}/auth-code/password`, {
+    return noAuthInstance.post(`phone/${phoneNumber}/auth-code/password`, {
         code: authCode
     });
 }
