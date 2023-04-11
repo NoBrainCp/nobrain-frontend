9a2f8886e048821781446ebe0ea465085f482425
junxtar <aa1870088@naver.com>
2023-04-11 15:02:35 +0900

    [#125] feat: userApi code refactor

--- src/api/user/userApi.js
+++ src/api/user/userApi.js
@@ -1,56 +1,49 @@
-import {instance, mail, user} from "../index";
+import {noAuthInstance, user} from "../index";
 
-export function signUpUser(userData) {
-    return instance.post('/sign-up', userData);
+export function signUpUser(userData) { //Api - 경로 변경
+    return noAuthInstance.post('/sign-up', userData);
 }
 
-export function signInUser(userData) {
-    return instance.post("/sign-in", userData);
-}
-export function existsUsername(username) {  //**
-    return instance.get(`/user/username/${username}/exists`);
+export function existsUsername(username) {  //v
+    return user.get(`/username/${username}/exists`);
 }
 
-export function existsLoginId(loginId) {
-    return instance.get(`/user/login-id/${loginId}/exists`);
+export function existsEmail(email) {    //v
+    return user.get(`/email/${email}/exists`);
 }
 
-export function existsEmail(email) {
-    return instance.get(`/email/${email}/exists`);
-}
-
-export function getUserInfo(username) {
+export function getUserInfo(username) { //v
     return user.get(`/${username}/info`);
 }
 
-export function getMyProfile() {
+export function getMyProfile() {    //v
     return user.get(`/my-profile`);
 }
 
-export function changeName(userId, username) {
-    return user.put(`/${userId}/username/${username}`);
+export function changeName(nameData) {  //v
+    return user.put(`/username`, nameData);
 }
 
-export function changePassword(passwordData) {
+export function changePassword(passwordData) {  //v
     return user.put(`/password`,passwordData);
 }
 
-export function changeForgotPassword(userData) {
-    return instance.put(`/user/forgot-password`, userData);
+export function resetPassword(resetPasswordData) { //**
+    return noAuthInstance.put(`/password/reset`, resetPasswordData);
 }
 
-export function changeProfileImage(profileImage) {
+export function changeProfileImage(profileImage) {  //v
     return user.put(`/profile-image`,
         {image: profileImage},
         {headers: {'Content-Type': 'multipart/form-data'}}
     );
 }
 
-export function deactivateAccount(userId) {
-    return user.delete(`/${userId}`);
+export function deactivateAccount(password) { //**
+    return user.delete(`/me`, password);
 }
 
-export function deleteProfileImage() {
+export function deleteProfileImage() { //v
     return user.delete(`/profile-image`);
 }
 
