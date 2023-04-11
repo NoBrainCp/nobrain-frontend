89637a7505aa6eda7de07d61f79e62428296556b
junxtar <aa1870088@naver.com>
2023-04-11 16:08:51 +0900

    [#126] feat: 이메일 체크 API 추가

--- src/api/user/userApi.js
+++ src/api/user/userApi.js
@@ -1,30 +1,34 @@
 import {noAuthInstance, user} from "../index";
 
-export function signUpUser(userData) { //Api - 경로 변경
+export function signUpUser(userData) {
     return noAuthInstance.post('/sign-up', userData);
 }
 
-export function existsUsername(username) {  //v
+export function noAuthExistsUsername(username) {
+    return noAuthInstance.get(`users/username/${username}/exists`);
+}
+
+export function existsEmail(email) {
+    return noAuthInstance.get(`users/email/${email}/exists`);
+}
+
+export function existsUsername(username) {
     return user.get(`/username/${username}/exists`);
 }
 
-export function existsEmail(email) {    //v
-    return user.get(`/email/${email}/exists`);
-}
-
-export function getUserInfo(username) { //v
+export function getUserInfo(username) {
     return user.get(`/${username}/info`);
 }
 
-export function getMyProfile() {    //v
+export function getMyProfile() {
     return user.get(`/my-profile`);
 }
 
-export function changeName(nameData) {  //v
+export function changeName(nameData) {
     return user.put(`/username`, nameData);
 }
 
-export function changePassword(passwordData) {  //v
+export function changePassword(passwordData) {
     return user.put(`/password`,passwordData);
 }
 
@@ -32,7 +36,7 @@ export function resetPassword(resetPasswordData) { //**
     return noAuthInstance.put(`/password/reset`, resetPasswordData);
 }
 
-export function changeProfileImage(profileImage) {  //v
+export function changeProfileImage(profileImage) {
     return user.put(`/profile-image`,
         {image: profileImage},
         {headers: {'Content-Type': 'multipart/form-data'}}
@@ -43,7 +47,7 @@ export function deactivateAccount(password) { //**
     return user.delete(`/me`, password);
 }
 
-export function deleteProfileImage() { //v
+export function deleteProfileImage() {
     return user.delete(`/profile-image`);
 }
 
