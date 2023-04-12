af147853ca65c514c1c2b759947e87877b7c8572
yoon <leeyt1201@gmail.com>
2023-04-12 17:33:41 +0900

    [#132] feat: local storiage util class 구현

---
+++ src/utils/storage.js
@@ -0,0 +1,55 @@
+export function saveUserIdToStorage(userId) {
+    localStorage.setItem('userId', userId);
+}
+
+export function saveUsernameToStorage(username) {
+    localStorage.setItem('username', username);
+}
+
+export function saveEmailToStorage(email) {
+    localStorage.setItem('email', email);
+}
+
+export function saveAccessTokenToStorage(accessToken) {
+    localStorage.setItem('accessToken', accessToken);
+}
+
+export function saveRefreshTokenToStorage(refreshToken) {
+    localStorage.setItem('refreshToken', refreshToken);
+}
+
+export function saveLoginUserInfoToStorage(userInfo) {
+    saveUserIdToStorage(userInfo.userId);
+    saveUsernameToStorage(userInfo.username);
+    saveEmailToStorage(userInfo.email);
+    saveAccessTokenToStorage(userInfo.tokenDto.accessToken);
+    saveRefreshTokenToStorage(userInfo.tokenDto.refreshToken);
+}
+
+export function getUserIdFromStorage() {
+    return localStorage.getItem('userId');
+}
+
+export function getUsernameFromStorage() {
+    return localStorage.getItem('username');
+}
+
+export function getEmailFromStorage() {
+    return localStorage.getItem('email');
+}
+
+export function getAccessTokenFromCookie() {
+    return localStorage.getItem('accessToken');
+}
+
+export function getRefreshTokenFromCookie() {
+    return localStorage.getItem('refreshToken');
+}
+
+export function getValueFromLocalStorage(key) {
+    return localStorage.getItem(key);
+}
+
+export function clearStorage() {
+    return localStorage.clear();
+}
